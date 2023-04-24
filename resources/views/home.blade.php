0a65c282811758e545f0dbca60ebb2b18307178a
varanustoken <varanustoken@gmail.com>
2023-04-25 06:36:56 +0700

    a

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,7 +12,7 @@
 A better way to present your money using fully featured digital currency
 </p>
 <a href="https://t.me/TryptoChain" class="btn btn-primary">JOIN US</a>
-<a href="https://poocoin.app/tokens/0x" class="btn btn-primary">CHART</a>
+<a href="https://poocoin.app/tokens/0x6Cb937a0B5e6C12f4ECeE0cAc473725c16ea2b54" class="btn btn-primary">CHART</a>
 </div>
 </div>
 </div>
